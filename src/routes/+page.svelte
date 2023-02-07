1e65ad1858e95e46812d0e895f94701d82802d62
Tommertom <Tom.Gruintjes@gmail.com>
2023-02-07 17:53:52 +0100

    code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { RefresherCustomEvent, RefresherEventDetail } from '@ionic/core';
+
+	function refresher(ev: RefresherEventDetail) {
+		console.log('Refreshed.', ev);
+		setTimeout(() => {
+			ev.target.complete();
+		}, 2000);
+	}
+</script>
+
+<ion-content class="ion-padding">
+	<ion-refresher slot="fixed" on:ionRefresh={refresher}>
+		<ion-refresher-content pulling-text="Pull to refresh" refreshing-text="Refreshing..." />
+	</ion-refresher>
+
+	<p>Pull this content down to trigger the refresh.</p>
+</ion-content>
